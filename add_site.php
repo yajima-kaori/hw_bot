86841ebf6552210038ec85581e8b5a3857efb635
yajimakaori <kaori.yajima19891029@gmail.com>
2016-05-07 03:56:01 +0000

    second commit

--- add_site.php
+++ add_site.php
@@ -1,3 +1,16 @@
 <?php
-var_dump($argv);
+
+require_once('config.php');
+require_once('functions.php');
+
+$data = $_SERVER['argv'];
+
+
+$dbh = connectDatabase();
+$sql = "insert into check_url(url) value(:url)";
+$stmt = $dbh->prepare($sql);
+$stmt->bindParam(":url",$data[1]);
+$stmt->execute();
+
+
 ?>
